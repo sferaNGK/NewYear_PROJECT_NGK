9f72365bb6a166052cd161dc78f0234d0a392f9a
FlipSiteX <verapushtrinova@gmail.com>
2023-12-04 11:46:16 +0300

    init

--- games/elka/script/elka.js
+++ games/elka/script/elka.js
@@ -61,13 +61,10 @@ function handleTouchMove(event) {
     if (currentElement.current !== null) { // --- ЕСЛИ ПЕРЕТАСКИВАЕМАЯ ЦЕЛЬ ОПРЕДЕЛЕНА
         let item = currentElement.current.target
 
-        let startTime = new Date().getTime()
-
         // --- ЗАДАЕМ ЧЕРЕЗ JS-АНИМАЦИЮ КООРДИНАТЫ НАШЕГО КУРСОРА (ПАЛЬЦА) НА ЭКРАНЕ ---
         window.requestAnimationFrame(() => {
-            let currTime = new Date().getTime()
-            item.style.left = event.touches[0].pageX - shiftX + 'px';
-            item.style.top = event.touches[0].pageY - shiftY + 'px';
+            item.style.left = (event.touches[0].pageX - shiftX)*100/document.documentElement.clientWidth + '%';
+            item.style.top = (event.touches[0].pageY - shiftY)*100/document.documentElement.clientHeight + '%';
 
             // --- ПРОВЕРЯЕМ, НЕ ВЫХОДИТ ЛИ НАШ ОБЪЕКТ ЗА ГРАНИЦЫ ЭКРАНА ---
             if (event.touches[0].pageX < 40) {
